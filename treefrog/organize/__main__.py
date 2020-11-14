28b88971985675aa1d217de04205bdacb00edfdd
Dawson Booth <github@dawsonbooth.com>
2020-11-14 12:20:14 -0600

    Fix argument parsing

--- treefrog/organize/__main__.py
+++ treefrog/organize/__main__.py
@@ -5,12 +5,15 @@ from . import Hierarchy, Tree
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Organize replay folder')
+    group = parser.add_mutually_exclusive_group()
     parser.add_argument('root_folder', type=Path,
                         help='Slippi folder root path')
     parser.add_argument('-c', '--netplay-code', type=str, required=True,
                         help='Netplay code (e.g. DTB#566)')
-    parser.add_argument('-f', '--flatten', action='store_true',
-                        help='Whether to flatten the folder hierarchy')
+    group.add_argument('-o', '--organize', action='store_true',
+                       help='Whether to organize the folder hierarchy')
+    group.add_argument('-f', '--flatten', action='store_true',
+                       help='Whether to flatten the folder hierarchy')
     parser.add_argument('-r', '--rename', action='store_true',
                         help='Whether to rename the files according to the game attributes')
     parser.add_argument('-p', '--show-progress', action='store_true',
@@ -31,7 +34,7 @@ if __name__ == "__main__":
 
     if args.flatten:
         tree.flatten(show_progress=args.show_progress)
-    else:
+    if args.organize:
         tree.organize(ordering, show_progress=args.show_progress)
     if args.rename:
         tree.rename(show_progress=args.show_progress)
